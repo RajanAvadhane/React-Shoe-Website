8f7658832f19cd3b22e81469bd8e9e4dcd9dd472
Rajan Avadhane <avadhanerajan9143@gmail.com>
2024-01-22 20:05:49 +0530

    First Commit

---
+++ vite-project/src/Components/Hero.jsx
@@ -0,0 +1,26 @@
+
+
+const Hero = () => {
+  return (
+    <main className='hero container'>
+      <div className='hero-content'>
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+      <div className="hero-btn">
+        <button className="primary-btn">Shop Now</button>
+        <button className="secondary-btn">Category</button>
+      </div> 
+       <div className="available">Also Available On</div>
+      <div className="brand-logos">
+        <img src="/Images/flipkart.png" alt="flipkart logo" />
+        <img src="/Images/amazon.png" alt="Amazon logo" />
+      </div>
+      </div>
+      <div className='hero-image'>
+        <img src="/Images/shoe_image.png" alt="shoe image" />
+      </div>
+    </main>
+  )
+}
+
+export default Hero
---
+++ vite-project/src/Components/Navigation.jsx
@@ -0,0 +1,20 @@
+
+const Navigation = () => {
+  return (
+    <nav className="container">
+      <div className=" logo"> 
+             <img src="/Images/brand_logo.png" alt="Brand Logo" />
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>Location</li>
+        <li>About</li>
+        <li>Contact</li>
+
+      </ul>
+      <button className="login-btn">Login</button>
+     </nav>
+  )
+}
+
+export default Navigation
